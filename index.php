8edfc0e101757cde2c4504d60953982b879a45df
mickael <mickael.morgado31@gmail.com>
2015-12-15 00:33:10 +0000

    hytek workflow

--- index.php
+++ index.php
@@ -14,7 +14,9 @@
 </head>
 <body>
 <?php 
-$link = mysqli_connect("localhost","root","","hytek_db");
+//$link = mysqli_connect("localhost","root","","hytek_db");
+$link = mysqli_connect("mysql.hostinger.pt","u206790186_hytek","Mickael01","u206790186_spbd");
+
 /* 
 	function to easy select single result:
 	$select = columns you want;
