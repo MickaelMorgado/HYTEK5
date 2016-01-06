b6c7ce886a0ca7d2712f6c657be9a613f182b3af
MickaelMorgado <mickael.morgado31@gmail.com>
2016-01-06 00:03:59 +0000

    upadte

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 	<link rel="stylesheet" type="text/css" href="stylesheets/styles3.css"/>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 	<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.3.15/slick.min.js"></script>
+	<script src="jquery.uploadify.min.js" type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="uploadify.css">
 
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -19,16 +21,16 @@
 
 
 <div class="container-fluid padded">
-			<form id="searchform" action="" method="GET">
-				<div class="GYForm">
-					<button onclick="google()" class="btn-search gg"><i class="fa fa-google"></i></button>
-					<button onclick="youtube()" class="btn-search yt"><i class="fa fa-youtube-play"></i></button>
-					<input id="searchinput" type="text" name="" placeholder="search">
-				</div>
-			</form>
+	<form id="searchform" action="" method="GET">
+		<div class="GYForm">
+			<input id="searchinput" type="text" name="" placeholder=" search"><div class="toggles-search-buttons">
+				<button onclick="google()" class="btn-search gg"><i class="fa fa-google"></i></button>
+				<button onclick="youtube()" class="btn-search yt"><i class="fa fa-youtube-play"></i></button>
+			</div>
+		</div>
+	</form>
 
 	<?php if(isset($_SESSION['id_session'])){ ?>
-	
 	
 		<div class="tabs">
 			<div id="tabs-results">
@@ -38,15 +40,49 @@
 			</div>
 		</div>
 
+		<div class="player-controls">
+			<span id="player-previous" title="previous"><i class="fa fa-fast-backward"></i></span>
+			<span id="player-pause" title="pause (s)"><i class="fa fa-pause"></i></span>
+			<span id="player-play" title="play (p)"><i class="fa fa-play"></i></span>
+			<span id="player-next" title="next"><i class="fa fa-fast-forward"></i></span>
+			<span id="player-playAt" title="go to first"><i class="fa fa-reply"></i></span>
+			<span class="play-pause"></span>
+			<span class="yt-time"></span>
+		</div>
+
 		<!--h1>most important tabs :</h1>
 		<ul class="list-unstyled">
 			<?php include('apps/mostimportanttabs.php'); ?>
 		</ul-->
 
-		<?php include('apps/youtube.php'); ?>
 		<!--?php include('apps/files.php'); ?-->
 
 		<!--?php include('settings.php'); ?-->
+
+		<div class="sidebar">
+			
+			<?php include('apps/youtube.php'); ?>
+
+			<h1>files</h1>
+			<form>
+				<div id="queue"></div>
+				<input id="file_upload" name="file_upload" type="file" multiple="true">
+			</form>
+			<script type="text/javascript">
+				<?php $timestamp = time();?>
+				$(function() {
+					$('#file_upload').uploadify({
+						'formData'     : {
+							'timestamp' : '<?php echo $timestamp;?>',
+							'token'     : '<?php echo md5('unique_salt' . $timestamp);?>'
+						},
+						'swf'      : 'uploadify.swf',
+						'uploader' : 'uploadify.php'
+					});
+				});
+			</script>
+
+		</div>
 
 	<?php }else{ ?>
 
