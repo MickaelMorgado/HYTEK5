864c1e090d2035e185dca52158312d3b66ec9c14
MickaelMorgado <mickael.morgado31@gmail.com>
2016-01-12 08:05:59 +0000

    remove file dbconnection path

--- apps/rmtab.php
+++ apps/rmtab.php
@@ -1,5 +1,5 @@
 <?php
-	@require_once('../../dbConnection.php');
+	@require_once('../dbConnection.php');
 	$tabsrmid = $_POST['tabId'];
 	if (isset($_POST['imp'])) {
 		mysqli_query($link, "DELETE FROM mostimportant WHERE id_mostimportant = $tabsrmid ");
