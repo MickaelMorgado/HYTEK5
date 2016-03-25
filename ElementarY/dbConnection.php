15e87a6bd30c66ad42e7436ca48875747b764dba
MickaelMorgado <mickael.morgado31@gmail.com>
2016-03-25 21:15:34 +0000

    Update dbConnection.php

--- ElementarY/dbConnection.php
+++ ElementarY/dbConnection.php
@@ -1,8 +1,8 @@
 <?php 
-//session_start();
-//error_reporting(0);
-//$link = mysqli_connect("mysql.hostinger.pt","u206790186_hytek","Mickael01","u206790186_spbd");
-//if (!$link) {
+session_start();
+error_reporting(0);
+$link = mysqli_connect("mysql.hostinger.pt","u206790186_hytek","Mickael01","u206790186_spbd");
+if (!$link) {
 	$link = mysqli_connect("localhost","root","","hytek_db"); 
-//}
+}
 ?>
