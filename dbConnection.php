e101c20d2e947886cfea4ae995e9a771ea935d8f
MickaelMorgado <mickael.morgado31@gmail.com>
2016-06-11 11:59:32 +0100

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -2,7 +2,7 @@
 	session_start();
 	error_reporting(0);
 
-	$link = mysqli_connect("mysql.hostinger.pt","u206790186_hytek","Mickael01","u206790186_spbd");
+	$link = mysqli_connect("mysql.hostinger.pt","u206790186_hytek","morgado","u206790186_spbd");
 	if (!$link) {
 		$link = mysqli_connect("localhost","root","","hytek_db"); 
 	}
