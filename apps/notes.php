1f1b56f7a8cee62e322ea61baa14efb1fb61c54e
MickaelMorgado <mickael.morgado31@gmail.com>
2016-07-30 16:33:55 +0100

    notes save fixed

--- apps/notes.php
+++ apps/notes.php
@@ -1,15 +1,18 @@
+<?php 
+	$sql1 = "SELECT * FROM notas WHERE id_session = $_SESSION[id_session]";
+	$sql2 = "INSERT INTO `notas` (`id_session`, `content`) VALUES ('$_SESSION[id_session];', 'no content')";
+?>
 <h2>Notes</h2>
 <form action="apps/notes/edit-note.php" method="POST">
-<textarea rows="7" cols="32" name="notetxtarea"><?php include('../../dbConnection.php'); $sql = "SELECT * FROM notes WHERE id_session = $_SESSION[id_session]";
-$result = mysqli_query($link,$sql);
-if ($result->num_rows > 0) {
-  while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['content'];
-  }   
-}else{
-  echo "no notes added";
-}
-  ?>
-  </textarea>
-  <input type="submit" value="edit" />
+	<textarea rows="7" cols="32" name="notetxtarea"><?php include('../../dbConnection.php'); 
+		$result = mysqli_query($link,$sql1);
+		if ($result->num_rows > 0) {
+			while ($row = mysqli_fetch_assoc($result)) { echo $row['content']; }   
+		}else{
+			mysqli_query($link,$sql2);
+		  	echo "no notes added";
+		}
+	  ?>
+	</textarea>
+	<input type="submit" value="edit" />
 </form>
--- apps/notes/edit-note.php
+++ apps/notes/edit-note.php
@@ -2,8 +2,8 @@
 	include('../../dbConnection.php');
 
 	$notetxtarea = $_POST['notetxtarea'];
-	echo "UPDATE notes SET content = '$notetxtarea' WHERE id_session = $_SESSION[id_session]";
-	$sql = "UPDATE notes SET content = '$notetxtarea' WHERE id_session = $_SESSION[id_session]";
+	echo "UPDATE `notas` SET `content` = '$notetxtarea' WHERE `id_session` = $_SESSION[id_session]";
+	$sql = "UPDATE `notas` SET `content` = '$notetxtarea' WHERE `id_session` = $_SESSION[id_session]";
 	mysqli_query($link, $sql); 
 	if ($debug!=true) { header("location: ../../");	}
 ?>
