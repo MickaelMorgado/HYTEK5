95c8c6910bf54d4e3bcc50e50c942c5ecb2528ff
MickaelMorgado <mickael.morgado31@gmail.com>
2016-11-06 15:35:35 +0000

    wallapperps

--- apps/chooseWallpapers.php
+++ apps/chooseWallpapers.php
@@ -1,17 +1,23 @@
+<?php 
+	include('../dbConnection.php');
+
+	$sql = "SELECT bg FROM settings";
+	$array = "http://s8.favim.com/orig/150705/crystal-growth-ice-gif-space-talking-shape-Favim.com-2904298.gif,http://img0.joyreactor.com/pics/post/full/river-hd-gif-nature-1314656.gif,http://bgfons.com/upload/ornaments_texture1141.jpg,http://wallpapercave.com/wp/5wjpNuT.jpg,https://wallpaperscraft.com/image/texture_black_pattern_light_lines_44980_1920x1080.jpg";
+
+	$result = mysqli_query($link,$sql);
+	if ($result->num_rows > 0) {
+		while ($row = mysqli_fetch_assoc($result)) {
+			$array = $array.",".$row['bg'];
+		} 	
+	}else{
+			$array = "http://s8.favim.com/orig/150705/crystal-growth-ice-gif-space-talking-shape-Favim.com-2904298.gif,http://img0.joyreactor.com/pics/post/full/river-hd-gif-nature-1314656.gif,http://bgfons.com/upload/ornaments_texture1141.jpg,http://wallpapercave.com/wp/5wjpNuT.jpg,https://wallpaperscraft.com/image/texture_black_pattern_light_lines_44980_1920x1080.jpg";
+	}	
+?>
 <h3>Choose a example wallpaper: </h3>
 <form action="apps/settings/setup.php" method="POST" class="wallpapers-form">
 	<?php 
-		$wallpapers = array("http://s8.favim.com/orig/150705/crystal-growth-ice-gif-space-talking-shape-Favim.com-2904298.gif",
-							"http://img0.joyreactor.com/pics/post/full/river-hd-gif-nature-1314656.gif",
-							"http://bgfons.com/upload/ornaments_texture1141.jpg",
-							"http://www.kemecer.com/i/oceanstormatnight-wallpaper-full-hd.jpg",
-							"https://wallpaperscraft.com/image/texture_black_pattern_light_lines_44980_1920x1080.jpg",
-							"http://s8.favim.com/orig/150705/crystal-growth-ice-gif-space-talking-shape-Favim.com-2904298.gif",
-							"http://img0.joyreactor.com/pics/post/full/river-hd-gif-nature-1314656.gif",
-							"http://bgfons.com/upload/ornaments_texture1141.jpg",
-							"http://www.kemecer.com/i/oceanstormatnight-wallpaper-full-hd.jpg",
-							"https://wallpaperscraft.com/image/texture_black_pattern_light_lines_44980_1920x1080.jpg");
-		foreach ($wallpapers as $value) {
+		$myArray = explode(',', $array);
+		foreach ($myArray as $value) {
 			?>
 				<label 
 					class="thumbnail-preview"
@@ -23,9 +29,8 @@
 					type="submit" 
 					class="btn btn-primary" 
 					id="<?php echo $value; ?>" 
-					value="<?php echo $value; ?>">
+					value="<?php echo $value; ?>" />
 			<?php
 		}
-
 	?>
 </form>
