84b24a113fd6cca8ef671ad3b1e203d7097e4e0f
MickaelMorgado <mickael.morgado31@gmail.com>
2016-11-27 17:07:48 +0000

    overflow fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	$error_reporting = 0;
 	include('dbConnection.php');
 ?>
-<html manifest="example.appcache">
+<html>
 <head>
 	<title>HYTEK5 - Start Page</title>
 	<meta name="theme-color" content="#111"><!--meta name="theme-color" content="#224455"-->
